92e04cb97ad2b445600a6ef3147033f06ba5bad7
bamsaali123 <alibasma289@gmail.com>
2024-09-16 04:50:28 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home:Custumer());//SplashScreen());
+    return MaterialApp(home:SplashScreen());
   }
 }
